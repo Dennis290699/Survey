08c4bd72bf7d7c08640d0ea9d5ca3168af8879df
Dennis290699 <88905858+Dennis290699@users.noreply.github.com>
2023-04-17 13:43:02 -0500

    Update Survey.py

--- Survey.py
+++ Survey.py
@@ -21,11 +21,14 @@ responses = [
     "Not at all concerned"
 ]
 
+# Prompt the user for the number of participants
+num_participants = int(input("Enter the number of participants: "))
+
 # Initialize a list to store the responses
 all_responses = []
 
-# Generate random responses for 200 people
-for i in range(400):
+# Generate random responses for the specified number of people
+for i in range(num_participants):
     print(f"Participant {i+1}")
     participant_responses = []
     for question in questions:
@@ -45,24 +48,33 @@ for participant_responses in all_responses:
     for response in participant_responses:
         counts[response] += 1
 
+# Print the number of each type of response
+for response, count in counts.items():
+    print(f"{response}: {count}")
+
 # Create a pie chart of the results
 labels = list(counts.keys())
 values = list(counts.values())
 plt.pie(values, labels=labels, autopct='%1.1f%%')
 plt.title("Survey Results")
-plt.show()
+
+# Save the pie chart to a specific directory
+plt.savefig("D:/curso-VisualCode/Python/Encuesta/survey_results.png")
+
+# Show the pie chart
+# plt.show()
 
 # Analyze the results using comparatives
-print("Compared to the other concerns, people are:")
+print("Compared to the other concerns in the survey, people are:")
 if counts["Very concerned"] > counts["Somewhat concerned"]:
-    print("More very concerned than somewhat concerned.")
+    print("More people are very concerned than somewhat concerned.")
 elif counts["Very concerned"] < counts["Somewhat concerned"]:
-    print("More somewhat concerned than very concerned.")
+    print("More people are somewhat concerned than very concerned.")
 else:
-    print("Equally very concerned and somewhat concerned.")
+    print("Equally many people are very concerned and somewhat concerned.")
 if counts["Not very concerned"] > counts["Not at all concerned"]:
-    print("More not very concerned than not at all concerned.")
+    print("More people are not very concerned than not at all concerned.")
 elif counts["Not very concerned"] < counts["Not at all concerned"]:
-    print("More not at all concerned than not very concerned.")
+    print("More people are not at all concerned than not very concerned.")
 else:
-    print("Equally not very concerned and not at all concerned.")
+    print("Equally many people are not very concerned and not at all concerned.")
